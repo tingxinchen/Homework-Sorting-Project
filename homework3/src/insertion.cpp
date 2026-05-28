0687fc8d2718c2f9b6dc44605393e4116884dfe8
chentingxin <41343128@nfu.edu.tw>
2026-05-28 19:16:02 +0800

    worst case

--- homework3/src/insertion.cpp
+++ homework3/src/insertion.cpp
@@ -1,44 +1,56 @@
 #include <iostream>
+#include <chrono>
+
 using namespace std;
 
-// Insertion Sort 函式
 void insertionSort(int arr[], int n)
 {
     // 從第二個元素開始
     for (int i = 1; i < n; i++)
     {
-        // 儲存目前要插入的值
+        // 儲存目前要的值
         int key = arr[i];
 
         // j 指向前一個元素
         int j = i - 1;
 
-        // 將比 key 大的元素往右移
+        // 將比key大的元素往右移
         while (j >= 0 && arr[j] > key)
         {
             arr[j + 1] = arr[j];
             j--;
         }
 
-        // 將 key 插入正確位置
         arr[j + 1] = key;
     }
 }
 
 int main()
 {
-    // 測試資料
-    int arr[] = { 5, 4, 3, 2, 1 };
+    int sizes[] = { 500, 1000, 2000, 3000, 4000, 5000 };
 
-    // 陣列大小
-    int n = 5;
+    for (int s = 0; s < 6; s++)
+    {
+        int n = sizes[s];
+        int* arr = new int[n];
+        // 倒序的測資
+        for (int i = 0; i < n; i++)
+            arr[i] = n - i;
 
-    // 呼叫 Insertion Sort
-    insertionSort(arr, n);
+        auto start = chrono::high_resolution_clock::now();
+        insertionSort(arr, n);
+        auto end = chrono::high_resolution_clock::now();
 
-    // 輸出排序結果
-    for (int i = 0; i < n; i++)
-        cout << arr[i] << " ";
+        double ms = chrono::duration<double, milli>(end - start).count();
+
+        cout << "n = " << n << " InsertionSort worst-case: " << ms << " ms" << endl;
+
+        bool sorted = true;
+        for (int i = 1; i < n; i++)
+            if (arr[i - 1] > arr[i]) sorted = false;
+
+        delete[] arr;
+    }
 
     return 0;
 }
